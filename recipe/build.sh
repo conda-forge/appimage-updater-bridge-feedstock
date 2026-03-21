cfd7b2c10fc5d3ac01fa4000330370e7f1f9f5ef
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-21 16:42:32 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,11 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CMAKE_ARGS=${CMAKE_ARGS//-DCMAKE_INSTALL_LIBDIR=lib/-DCMAKE_INSTALL_LIBDIR=plugins}
+
 cmake -S ${SRC_DIR} -B build -G "Ninja" \
     -D CMAKE_POLICY_VERSION_MINIMUM=3.5 \
     -D CMAKE_BUILD_TYPE=Release \
-    -D CMAKE_INSTALL_LIBDIR:FILEPATH=$PREFIX/plugins \
     -D BUILD_AS_PLUGIN:BOOL=ON \
     ${CMAKE_ARGS}
 
